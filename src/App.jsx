94f2af1cdf837947d25fac9668d7072c65c6054c
Rachel Little <racheldawnlittle@gmail.com>
2025-02-09 12:53:37 -0700

    basic home page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import TopNav from "./components/TopNav";
+import Container from "@mui/material/Container";
+import Hero from "./components/Hero";
+import Listings from "./components/Listings";
 
 function App() {
-
   return (
     <>
-<h1>happy tails rescue hub</h1>
+      <Container maxWidth="xl" disableGutters>
+        <TopNav />
+        <Hero />
+        <Listings />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Listings.jsx
@@ -0,0 +1,12 @@
+import Container from "@mui/material/Container";
+
+
+function Listings() {
+    return (
+        <Container maxWidth="xl">
+            <h1>Listings</h1>
+        </Container>
+    );
+}
+
+export default Listings;
